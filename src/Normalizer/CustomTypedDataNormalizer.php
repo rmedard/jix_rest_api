03a398156905046d5562d478b66555d380c4c1aa
rmedard <00Derfine@5>
2018-11-30 00:31:12 +0100

    W10

--- src/Normalizer/CustomTypedDataNormalizer.php
+++ src/Normalizer/CustomTypedDataNormalizer.php
@@ -9,28 +9,12 @@
 namespace Drupal\jir_rest_api\Normalizer;
 
 use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\hal\Normalizer\ContentEntityNormalizer;
-use Drupal\node\NodeInterface;
 use Symfony\Component\Serializer\Exception\CircularReferenceException;
 use Symfony\Component\Serializer\Exception\InvalidArgumentException;
 use Symfony\Component\Serializer\Exception\LogicException;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class CustomTypedDataNormalizer extends ContentEntityNormalizer {
-
-    /**
-     * Checks whether the given class is supported for normalization by this normalizer.
-     *
-     * @param mixed $data Data to normalize
-     * @param string $format The format being (de-)serialized from or into
-     *
-     * @return bool
-     */
-//    public function supportsNormalization($data, $format = null)
-//    {
-//        return $data instanceof ContentEntityInterface;
-//    }
 
     /**
      * Normalizes an object into a set of arrays/scalars.
@@ -49,16 +33,16 @@ class CustomTypedDataNormalizer extends ContentEntityNormalizer {
     public function normalize($object, $format = null, array $context = array())
     {
         $attributes = parent::normalize($object, $format, $context);
-        $changed_timestamp = $object->getChangedTime();
-        $created_timestamp = $object->getCreatedTime();
-
-        $changed_date = DrupalDateTime::createFromTimestamp($changed_timestamp);
-        $created_date = DrupalDateTime::createFromTimestamp($created_timestamp);
-
-        $attributes['changed_iso8601'] = $changed_date->format('d-m-Y H:i:s');
-        $attributes['created_iso8601'] = $created_date->format('d-m-Y H:i:s');
-        $attributes['link'] = $object->toUrl()->toString();
-        ksort($attributes);
+//        $changed_timestamp = $object->getChangedTime();
+//        $created_timestamp = $object->getCreatedTime();
+//
+//        $changed_date = DrupalDateTime::createFromTimestamp($changed_timestamp);
+//        $created_date = DrupalDateTime::createFromTimestamp($created_timestamp);
+//
+//        $attributes['changed_iso8601'] = $changed_date->format('d-m-Y H:i:s');
+//        $attributes['created_iso8601'] = $created_date->format('d-m-Y H:i:s');
+//        $attributes['link'] = $object->toUrl()->toString();
+//        ksort($attributes);
 
         \Drupal::logger('jix_rest_api')->debug('normalizer called...');
         return $attributes;
