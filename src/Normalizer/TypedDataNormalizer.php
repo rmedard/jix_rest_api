9d39b17bf4d51a71b6700e59dc0876aac374b1ff
rmedard <00Derfine@5>
2018-11-30 01:27:32 +0100

    W10

---
+++ src/Normalizer/TypedDataNormalizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: medard
+ * Date: 26/03/2017
+ * Time: 15:32
+ */
+
+namespace Drupal\jir_rest_api\Normalizer;
+
+
+use Drupal\serialization\Normalizer\NormalizerBase;
+
+class TypedDataNormalizer extends NormalizerBase {
+
+    protected $supportedInterfaceOrClass = 'Drupal\node\NodeInterface';
+
+    public function normalize($object, $format = NULL, array $context = array()){
+
+        $value = $object->getValue();
+        if (isset($value[0]) && isset($value[0]['value'])) {
+            $value = $value[0]['value'];
+        }
+        return $value;
+    }
+}
